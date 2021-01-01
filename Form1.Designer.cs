dfa25f8d36fa97c49ec27c518d7570c9abbae1a4
Luis Carvalho <luiscarvalho239@gmail.com>
2021-01-01 12:45:45 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace WV2Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.CreationProperties = null;
+            this.webView.Location = new System.Drawing.Point(12, 32);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(766, 402);
+            this.webView.TabIndex = 0;
+            this.webView.Text = "webView";
+            this.webView.ZoomFactor = 1D;
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(12, 6);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(726, 20);
+            this.tbUrl.TabIndex = 1;
+            this.tbUrl.TextChanged += new System.EventHandler(this.tbUrl_TextChanged);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(744, 6);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(34, 20);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(154, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Show HTML page into WV";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(311, 440);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 25);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 471);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace WV2Example
+{
+    public partial class Form1 : Form
+    {
+        private readonly int modeSSL = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            Resize += new EventHandler(Form_Resize);
+            // webView.NavigationStarting += EnsureHttps;
+            InitializeAsync();
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new Size(webView.Location);
+            btnGo.Left = this.ClientSize.Width - btnGo.Width;
+            tbUrl.Width = btnGo.Left - tbUrl.Left;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbUrl.Text = "localhost.loc";
+            SetProtocolInURL(modeSSL);
+            webView.Source = new Uri(tbUrl.Text);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            SetProtocolInURL(modeSSL);
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(tbUrl.Text);
+            }
+        }
+
+        string SetProtocolInURL(int enableSSL = 0)
+        {
+            var protonorm = @"http://";
+            var protossl = @"https://";
+
+            tbUrl.Text = tbUrl.Text.Replace(protossl, "");
+            tbUrl.Text = tbUrl.Text.Replace(protonorm, "");
+
+            if (enableSSL == 1)
+            {
+                if (!tbUrl.Text.StartsWith(protossl))
+                {
+                    tbUrl.Text = protossl + tbUrl.Text;
+                }
+            }
+            else
+            {
+                if (!tbUrl.Text.StartsWith(protonorm))
+                {
+                    tbUrl.Text = protonorm + tbUrl.Text;
+                }
+            }
+
+            return tbUrl.Text;
+        }
+
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => console.log(event.data));");
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            tbUrl.Text = uri;
+            webView.CoreWebView2.PostWebMessageAsString(uri);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                string projectDir = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\.."));
+                string file = projectDir + @"/assets/pages/mytemplate.html";
+
+                if (webView != null && webView.CoreWebView2 != null)
+                {
+                    tbUrl.Text = "about:blank";
+                    webView.CoreWebView2.NavigateToString(File.ReadAllText(file));
+                }
+            }
+            else
+            {
+                if (webView != null && webView.CoreWebView2 != null)
+                {
+                    tbUrl.Text = "localhost.loc";
+                    SetProtocolInURL(modeSSL);
+                    webView.CoreWebView2.Navigate(tbUrl.Text);
+                }
+            }
+        }
+
+        private void tbUrl_TextChanged(object sender, EventArgs e)
+        {
+            return;
+        }
+
+        //void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        //{
+        //    String uri = args.Uri;
+        //    if (!uri.StartsWith("https://"))
+        //    {
+        //        webView.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+        //        args.Cancel = true;
+        //    }
+        //}
+    }
+}
